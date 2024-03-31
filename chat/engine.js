bc608d16e3d4cb8089d64f2879011cf2edc0de73
rith1x <kiruthikkumarmpc24@gmail.com>
2024-03-31 12:20:48 +0530

    main

---
+++ chat/engine.js
@@ -0,0 +1,15 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSFs3HWHk1Q1anaPezWvkcVRVxQckQEzc",
+    authDomain: "chat-e-1e0a6.firebaseapp.com",
+    projectId: "chat-e-1e0a6",
+    storageBucket: "chat-e-1e0a6.appspot.com",
+    messagingSenderId: "702252242317",
+    appId: "1:702252242317:web:b1940f3ef6461c33fed9c4"
+};
+
+ let c =firebase.initializeApp(firebaseConfig);
+console.log(c);
+
+
