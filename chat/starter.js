6586f37a5b2cd6ddb799a5275eff36c8fb19ba42
rith1x <kiruthikkumarmpc24@gmail.com>
2024-05-05 11:41:46 +0530

    msg

--- chat/starter.js
+++ chat/starter.js
@@ -2,3 +2,4 @@ const lklk = {
     [xy]: (_ms,_ky) => { return CryptoJS.AES.encrypt(_ms,_ky).toString(); },
     [yx]: (_ms,_ky) => { return CryptoJS.AES.decrypt(_ms,_ky).toString(CryptoJS.enc.Utf8); }
 };
+const a = process.env
