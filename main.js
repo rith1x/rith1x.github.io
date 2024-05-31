2364c64bbff6feefb3bd4aa564f57be59466b750
rith1x <kiruthikkumarmpc24@gmail.com>
2024-05-31 07:08:20 +0530

    num

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+const h1 = document.getElementById("h1"),
+    banner = document.getElementById("banner"),
+    button = document.getElementById("button");
+
+const onScroll = (event) => {
+    const scrollPosition = event.target.scrollingElement.scrollTop;
+    if (scrollPosition > 75) {
+        banner.style.backgroundSize = "200%";
+        h1.style.opacity = 0;
+        h1.style.translate = "0 -50px";
+        h1.style.scale = "0.9";
+        button.style.opacity = 0;
+        button.style.translate = "0 -50px";
+        button.style.scale = "0.8";
+    } else {
+        banner.style.backgroundSize = "180%";
+        h1.style.opacity = 1;
+        h1.style.translate = 0;
+        h1.style.scale = 1;
+        button.style.opacity = 1;
+        button.style.translate = 0;
+        button.style.scale = 1;
+    }
+};
+
+document.addEventListener("scroll", onScroll);
